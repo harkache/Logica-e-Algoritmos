dd14b88e4b348a40ad509b39c2e960d6d29300a5
João Victor <jvnezzes@gmail.com>
2025-05-07 11:14:02 -0300

    Add files via upload

---
+++ Maio/07-05 (Quarta-feira)/code-da-atividade/exercise.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (){
+    int n;
+
+    system("cls");
+    printf("Ola! Por favor, logo abaixo, digite o numero de linhas que o triangulo contera.\n");
+    scanf("%d", &n);
+}
